b7c69eb6e701462ac097683e52c2083859a95cf2
Steve Aldworth <s.aldworth@tvdsb.ca>
2020-03-09 11:56:15 -0400

    Add project files.

---
+++ Keyboard Events CSharp/Form1.Designer.cs
@@ -0,0 +1,64 @@
+﻿namespace Keyboard_Events_CSharp
+{
+    partial class FormKeyboard
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblKeyInfo = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblKeyInfo
+            // 
+            this.lblKeyInfo.AutoSize = true;
+            this.lblKeyInfo.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblKeyInfo.Location = new System.Drawing.Point(42, 33);
+            this.lblKeyInfo.Name = "lblKeyInfo";
+            this.lblKeyInfo.Size = new System.Drawing.Size(209, 25);
+            this.lblKeyInfo.TabIndex = 0;
+            this.lblKeyInfo.Text = "No Event Detected";
+            // 
+            // FormKeyboard
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(352, 120);
+            this.Controls.Add(this.lblKeyInfo);
+            this.Name = "FormKeyboard";
+            this.Text = "Keyboard Events";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.FormKeyboard_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.FormKeyboard_KeyUp);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblKeyInfo;
+    }
+}
+
---
+++ Keyboard Events CSharp/Form1.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Keyboard_Events_CSharp
+{
+    public partial class FormKeyboard : Form
+    {
+        public FormKeyboard()
+        {
+            InitializeComponent();
+        }
+
+        private void FormKeyboard_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Up)
+                lblKeyInfo.Text = "Up Arrow Pressed";
+            else if (e.KeyCode == Keys.Down)
+                lblKeyInfo.Text = "Down Arrow Pressed";
+            else if (e.KeyCode == Keys.Left)
+                lblKeyInfo.Text = "Left Arrow Pressed";
+            else if (e.KeyCode == Keys.Right)
+                lblKeyInfo.Text = "Right Arrow Pressed";
+            else if (e.KeyCode == Keys.B)
+            {
+                lblKeyInfo.Text = "B Key Pressed";
+                this.BackColor = Color.LightBlue;
+            }
+            else if (e.KeyCode == Keys.R)
+            {
+                lblKeyInfo.Text = "R Key Pressed";
+                this.BackColor = Color.Red;
+            }
+            else if (e.KeyCode == Keys.Y)
+            {
+                lblKeyInfo.Text = "Y Key Pressed";
+                this.BackColor = Color.Yellow;
+            }
+            else if (e.KeyCode == Keys.G)
+            {
+                lblKeyInfo.Text = "G Key Pressed";
+                this.BackColor = Color.Green;
+            }
+            else
+                lblKeyInfo.Text = "Irrelevent Key Pressed";
+        }
+
+        private void FormKeyboard_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Up)
+                lblKeyInfo.Text = "Up Arrow Released";
+            else if (e.KeyCode == Keys.Down)
+                lblKeyInfo.Text = "Down Arrow Released";
+            else if (e.KeyCode == Keys.Left)
+                lblKeyInfo.Text = "Left Arrow Released";
+            else if (e.KeyCode == Keys.Right)
+                lblKeyInfo.Text = "Right Arrow Released";
+            else if (e.KeyCode == Keys.B)
+            {
+                lblKeyInfo.Text = "B Key Released";
+                this.BackColor = Form.DefaultBackColor;
+            }
+            else if (e.KeyCode == Keys.R)
+            {
+                lblKeyInfo.Text = "R Key Released";
+                this.BackColor = Form.DefaultBackColor;
+            }
+            else if (e.KeyCode == Keys.Y)
+            {
+                lblKeyInfo.Text = "Y Key Released";
+                this.BackColor = Form.DefaultBackColor;
+            }
+            else if (e.KeyCode == Keys.G)
+            {
+                lblKeyInfo.Text = "G Key Released";
+                this.BackColor = Form.DefaultBackColor;
+            }
+            else
+                lblKeyInfo.Text = "Irrelevent Key Released";
+        }
+    }
+}
